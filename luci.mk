b5146d65da7f8a7d1ec8e1b413616432475f735c
ikghx <admin@ikghx.com>
2020-01-24 13:46:34 +0800

    Fixed default luci version number

--- luci.mk
+++ luci.mk
@@ -62,7 +62,7 @@ PKG_VERSION?=$(if $(DUMP),x,$(strip $(shell \
 			set -- $$(git log -1 --format="%ct %h" --abbrev=7); \
 			secs="$$(($$1 % 86400))"; \
 			yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
-			revision="$$(printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2")"; \
+			revision="$$(printf 'openwrt-life-1907')"; \
 		fi; \
 	else \
 		revision="unknown"; \
