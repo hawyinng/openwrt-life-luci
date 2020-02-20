7d6895512c0202e16f3bc21c1757631c922b491f
ikghx <admin@ikghx.com>
2020-02-20 22:08:34 +0800

    luci-app-passwall: fix

--- applications/luci-app-passwall/luasrc/model/cbi/passwall/other.lua
+++ applications/luci-app-passwall/luasrc/model/cbi/passwall/other.lua
@@ -129,8 +129,8 @@ o.rmempty = false
 ---- Hide Menu
 o = s:option(Button, "hide", translate("Hide Menu"),
              translate(
-                 "After the hidden to the display, input example in the address bar：") ..
-                 "http://192.168.9.1/cgi-bin/luci/admin/vpn/passwall/show")
+                 "After the hidden to the display, input example in the address bar:") ..
+                 " http://192.168.9.1/cgi-bin/luci/admin/vpn/passwall/show")
 o.inputstyle = "remove"
 function o.write(e, e)
     luci.http.redirect(luci.dispatcher.build_url("admin", "vpn", "passwall", "hide"))
