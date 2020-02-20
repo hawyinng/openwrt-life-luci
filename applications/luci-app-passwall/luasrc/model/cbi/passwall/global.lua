232f10e374d5608c1e37418dc645c0c04fe86aae
ikghx <admin@ikghx.com>
2020-02-20 22:09:14 +0800

    luci-app-passwall: fix

--- applications/luci-app-passwall/luasrc/model/cbi/passwall/global.lua
+++ applications/luci-app-passwall/luasrc/model/cbi/passwall/global.lua
@@ -37,7 +37,7 @@ table.sort(key_table)
 m = Map(appname)
 local status_use_big_icon = api.uci_get_type("global_other",
                                              "status_use_big_icon", 1)
-if status_use_big_icon and status_use_big_icon == "1" then
+if status_use_big_icon and tonumber(status_use_big_icon) == 1 then
     m:append(Template("passwall/global/status"))
 else
     m:append(Template("passwall/global/status2"))
@@ -53,7 +53,7 @@ o = s:option(Flag, "enabled", translate("Enable"))
 o.rmempty = false
 
 ---- TCP Node
-local tcp_node_num = api.uci_get_type("global_other", "tcp_node_num", 1)
+local tcp_node_num = tonumber(api.uci_get_type("global_other", "tcp_node_num", 1))
 for i = 1, tcp_node_num, 1 do
     if i == 1 then
         o = s:option(ListValue, "tcp_node" .. i, translate("TCP Node"),
@@ -67,7 +67,7 @@ for i = 1, tcp_node_num, 1 do
 end
 
 ---- UDP Node
-local udp_node_num = api.uci_get_type("global_other", "udp_node_num", 1)
+local udp_node_num = tonumber(api.uci_get_type("global_other", "udp_node_num", 1))
 for i = 1, udp_node_num, 1 do
     if i == 1 then
         o = s:option(ListValue, "udp_node" .. i, translate("UDP Node"),
@@ -84,7 +84,7 @@ for i = 1, udp_node_num, 1 do
 end
 
 ---- Socks5 Node
-local socks5_node_num = api.uci_get_type("global_other", "socks5_node_num", 1)
+local socks5_node_num = tonumber(api.uci_get_type("global_other", "socks5_node_num", 1))
 for i = 1, socks5_node_num, 1 do
     if i == 1 then
         o = s:option(ListValue, "socks5_node" .. i, translate("Socks5 Node"),
