3992ec2146b752cacb0f0f2e05779ae03a18182e
ikghx <admin@ikghx.com>
2020-03-03 22:11:13 +0800

    luci-app-syncthing: fix

--- applications/luci-app-syncthing/luasrc/model/cbi/syncthing.lua
+++ applications/luci-app-syncthing/luasrc/model/cbi/syncthing.lua
@@ -11,20 +11,31 @@ s.anonymous = true
 o = s:option(Flag, "enabled", translate("Enable"))
 o.rmempty = false
 
+o = s:option(ListValue, "user", translate("Run daemon as user"))
+o:value("")
+local user
+for user in luci.util.execi("cat /etc/passwd | cut -d':' -f1") do
+	o:value(user)
+end
+
 o = s:option(Value, "gui_address", translate("Listening address"))
 o.datatype = "ipaddrport"
 o.placeholder = "0.0.0.0:8384"
 o.default = "0.0.0.0:8384"
 o.rmempty = false
 
-o = s:option(Value, "home", translate("Home directory"))
+o = s:option(Value, "home", translate("Configuration directory"))
 o.placeholder = "/etc/syncthing/"
 o.default = "/etc/syncthing/"
 o.rmempty = false
 
-o = s:option(Value, "logfile", translate("Log file"))
-o.placeholder = "/var/log/syncthing.log"
-o.default = "/var/log/syncthing.log"
+o = s:option(Value, "nice", translate("Scheduling priority"), translate("Set the scheduling priority of the spawned process."))
+o.datatype = "range(-20,19)"
+o.default = "19"
+o.rmempty = false
+
+o = s:option(Value, "macprocs", translate("Concurrent threads"), translate("0 to match the number of CPUs (default)"))
+o.default = "0"
 o.rmempty = false
 
 return m
