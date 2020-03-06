dd3256456295219cd7fa2ddfc73ac0baf07342a4
ikghx <admin@ikghx.com>
2020-03-06 21:08:19 +0800

    luci-app-unblockmusic: fix

--- applications/luci-app-unblockmusic/luasrc/controller/unblockmusic.lua
+++ applications/luci-app-unblockmusic/luasrc/controller/unblockmusic.lua
@@ -6,7 +6,7 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "unblockmusic"},firstchild(), _("解锁网易云灰色歌曲")).dependent = true
+	entry({"admin", "services", "unblockmusic"},firstchild(), _("解锁网易云灰色歌曲")).dependent = false
 	entry({"admin", "services", "unblockmusic", "general"},cbi("unblockmusic"), _("基本设置"), 1)
 	entry({"admin", "services", "unblockmusic", "log"},form("unblockmusiclog"), _("日志"), 2)
 	entry({"admin", "services", "unblockmusic", "status"},call("act_status")).leaf=true
@@ -14,7 +14,7 @@ end
 
 function act_status()
   local e={}
-  e.running=luci.sys.call("busybox ps -w | grep UnblockNeteaseMusic/app.js | grep -v grep >/dev/null")==0
+  e.running=luci.sys.call("busybox ps -w | grep UnblockNeteaseMusic | grep -v grep >/dev/null")==0
   luci.http.prepare_content("application/json")
   luci.http.write_json(e)
 end
