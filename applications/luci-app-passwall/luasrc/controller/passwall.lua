336565251513b3205d8be58334dd4067919babb9
ikghx <admin@ikghx.com>
2020-03-08 09:22:09 +0800

    luci-app-passwall: fix

--- applications/luci-app-passwall/luasrc/controller/passwall.lua
+++ applications/luci-app-passwall/luasrc/controller/passwall.lua
@@ -2,6 +2,7 @@
 module("luci.controller.passwall", package.seeall)
 local appname = "passwall"
 local http = require "luci.http"
+local passwall = require "luci.model.cbi.passwall.api.passwall"
 local kcptun = require "luci.model.cbi.passwall.api.kcptun"
 local brook = require "luci.model.cbi.passwall.api.brook"
 local v2ray = require "luci.model.cbi.passwall.api.v2ray"
@@ -113,7 +114,7 @@ function status()
                                          "ps -w | grep -v grep | grep '%s/bin/' | grep haproxy >/dev/null",
                                          appname)) == 0
     local tcp_node_num = luci.sys.exec(
-                             "echo -n `uci -q get %s.@global_other[0].tcp_node_num`" %
+                             "echo -n $(uci -q get %s.@global_other[0].tcp_node_num)" %
                                  appname)
     for i = 1, tcp_node_num, 1 do
         e["kcptun_tcp_node%s_status" % i] =
@@ -127,7 +128,7 @@ function status()
     end
 
     local udp_node_num = luci.sys.exec(
-                             "echo -n `uci -q get %s.@global_other[0].udp_node_num`" %
+                             "echo -n $(uci -q get %s.@global_other[0].udp_node_num)" %
                                  appname)
     for i = 1, udp_node_num, 1 do
         e["udp_node%s_status" % i] = luci.sys.call(
@@ -137,7 +138,7 @@ function status()
     end
 
     local socks5_node_num = luci.sys.exec(
-                                "echo -n `uci -q get %s.@global_other[0].socks5_node_num`" %
+                                "echo -n $(uci -q get %s.@global_other[0].socks5_node_num)" %
                                     appname)
     for i = 1, socks5_node_num, 1 do
         e["kcptun_socks_node%s_status" % i] =
@@ -159,11 +160,11 @@ function connect_status()
     local start_time = os.time()
     if luci.http.formvalue("type") == "google" then
         e.status = luci.sys.call(
-                       "echo `/usr/share/passwall/test.sh test_url 'www.google.com'` | grep 200 >/dev/null") ==
+                       "echo $(/usr/share/passwall/test.sh test_url 'www.google.com') | grep 200 >/dev/null") ==
                        0
     else
         e.status = luci.sys.call(
-                       "echo `/usr/share/passwall/test.sh test_url 'www.baidu.com'` | grep 200 >/dev/null") ==
+                       "echo $(/usr/share/passwall/test.sh test_url 'www.baidu.com') | grep 200 >/dev/null") ==
                        0
     end
     local use_time = os.difftime(os.time(), start_time)
@@ -178,18 +179,18 @@ function ping_node()
     local port = luci.http.formvalue("port")
     local e = {}
     e.index = index
-    if luci.sys.exec("echo -n `uci -q get %s.@global_other[0].use_tcping`" %
+    if luci.sys.exec("echo -n $(uci -q get %s.@global_other[0].use_tcping)" %
                          appname) == "1" and
         luci.sys.exec("echo -n $(command -v tcping)") ~= "" then
-        luci.sys.call(string.format(
-                          "ps -w | grep 'tcping -q -c 1 -i 1 -p %s %s' | grep -v grep | awk '{print $1}' | xargs kill -9 > /dev/null",
-                          port, address))
         e.ping = luci.sys.exec(string.format(
-                                   "echo -n $(tcping -q -c 1 -i 1 -p %s %s 2>&1 | grep -o 'time=[0-9]*' | awk -F '=' '{print$2}')",
+                                   "echo -n $(tcping -q -c 1 -i 1 -p %s %s 2>&1 | grep -o 'time=[0-9]*' | awk -F '=' '{print$2}') 2>/dev/null",
                                    port, address))
+        luci.sys.call(string.format(
+                          "ps -w | grep 'tcping -q -c 1 -i 1 -p %s %s' | grep -v grep | awk '{print $1}' | xargs kill -9 2>/dev/null",
+                          port, address))
     else
         e.ping = luci.sys.exec(
-                     "echo -n `ping -c 1 -W 1 %q 2>&1 | grep -o 'time=[0-9]*' | awk -F '=' '{print$2}'`" %
+                     "echo -n $(ping -c 1 -W 1 %q 2>&1 | grep -o 'time=[0-9]*' | awk -F '=' '{print$2}') 2>/dev/null" %
                          address)
     end
     luci.http.prepare_content("application/json")
@@ -230,8 +231,8 @@ function check_port()
             (s.v2ray_transport and s.v2ray_transport == "mkcp" and s.port) then
         else
             local type = s.type
-            if type and type ~= "V2ray_balancing" and s.address and s.port and
-                s.remarks then
+            if type and type ~= "V2ray_balancing" and type ~= "V2ray_shunt" and
+                s.address and s.port and s.remarks then
                 node_name = "%s：[%s] %s:%s" %
                                 {s.type, s.remarks, s.address, s.port}
                 tcp_socket = nixio.socket("inet", "stream")
