f41d946cc7f8606f4a55b7d5ddb6ece425c6aa35
ikghx <admin@ikghx.com>
2020-03-15 01:14:46 +0800

    luci-app-rsyncd: fix

--- applications/luci-app-rsyncd/luasrc/model/cbi/rsyncd.lua
+++ applications/luci-app-rsyncd/luasrc/model/cbi/rsyncd.lua
@@ -16,6 +16,8 @@ s.anonymous = true
 
 s:tab("general", translate("General Settings"))
 s:tab("conf1", translate("Server configuration"))
+s:tab("conf2", translate("Account list"))
+s:tab("conf3", translate("Password file"))
 
 en = s:taboption("general", Flag, "enabled", translate("Enabled"))
 en.rmempty = false
@@ -32,6 +34,32 @@ end
 cf1.write = function(self, section, value)
 	fs.writefile(file1, value:gsub("\r\n", "\n"))
     luci.util.exec("/etc/init.d/rsyncd restart >/dev/null 2>&1")
+end
+
+local file2 = "/etc/rsyncd.secrets"
+cf2 = s:taboption("conf2", Value, "_tmp2")
+cf2.description = translate("This is the content of the file '/etc/rsyncd.secrets'. Make changes as needed.")
+cf2.template = "cbi/tvalue"
+cf2.rows = 20
+cf2.wrap = "off"
+cf2.cfgvalue = function(self, section)
+	return fs.readfile(file2) or ""
+end
+cf2.write = function(self, section, value)
+	fs.writefile(file2, value:gsub("\r\n", "\n"))
+end
+
+local file3 = "/etc/rsyncd.password"
+cf3 = s:taboption("conf3", Value, "_tmp3")
+cf3.description = translate("This is the content of the file '/etc/rsyncd.password'. Make changes as needed.")
+cf3.template = "cbi/tvalue"
+cf3.rows = 20
+cf3.wrap = "off"
+cf3.cfgvalue = function(self, section)
+	return fs.readfile(file3) or ""
+end
+cf3.write = function(self, section, value)
+	fs.writefile(file3, value:gsub("\r\n", "\n"))
 end
 
 return m
