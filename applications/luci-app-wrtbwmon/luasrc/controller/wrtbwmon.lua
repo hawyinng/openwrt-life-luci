358a35c2e614bb3ebd13ba41af260aa2cb8a37a8
ikghx <admin@ikghx.com>
2020-03-21 17:19:20 +0800

    luci-app-wrtbwmon: fix

--- applications/luci-app-wrtbwmon/luasrc/controller/wrtbwmon.lua
+++ applications/luci-app-wrtbwmon/luasrc/controller/wrtbwmon.lua
@@ -1,13 +1,13 @@
 module("luci.controller.wrtbwmon", package.seeall)
 
 function index()
-    entry({"admin", "nlbw", "usage"}, alias("admin", "nlbw", "usage", "details"), _("Usage"), 60)
-    entry({"admin", "nlbw", "usage", "details"}, template("wrtbwmon"), _("Details"), 10).leaf=true
-    entry({"admin", "nlbw", "usage", "config"}, cbi("wrtbwmon/config"), _("Configuration"), 20).leaf=true
-    entry({"admin", "nlbw", "usage", "custom"}, form("wrtbwmon/custom"), _("User file"), 30).leaf=true
-    entry({"admin", "nlbw", "usage", "check_dependency"}, call("check_dependency")).dependent=true
-    entry({"admin", "nlbw", "usage", "usage_data"}, call("usage_data")).dependent=true
-    entry({"admin", "nlbw", "usage", "usage_reset"}, call("usage_reset")).dependent=true
+    entry({"admin", "network", "usage"}, alias("admin", "network", "usage", "details"), _("Usage"), 60)
+    entry({"admin", "network", "usage", "details"}, template("wrtbwmon"), _("Details"), 10).leaf=true
+    entry({"admin", "network", "usage", "config"}, cbi("wrtbwmon/config"), _("Configuration"), 20).leaf=true
+    entry({"admin", "network", "usage", "custom"}, form("wrtbwmon/custom"), _("User file"), 30).leaf=true
+    entry({"admin", "network", "usage", "check_dependency"}, call("check_dependency")).dependent=true
+    entry({"admin", "network", "usage", "usage_data"}, call("usage_data")).dependent=true
+    entry({"admin", "network", "usage", "usage_reset"}, call("usage_reset")).dependent=true
 end
 
 function usage_database_path()
